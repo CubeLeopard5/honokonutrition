3fe663d3d6ddb1b1eb7401e38893a8f07670c80d
Adrien Michaud <adrien.michaud@epitech.eu>
2023-09-11 17:31:12 +0200

    init repo

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <NuxtLink to="/logins/hue-rotate"> To login rotate </NuxtLink>
+        <NuxtLink to="/navbars/design-one"> To navbar blue </NuxtLink>
+    </div>
+</template>
