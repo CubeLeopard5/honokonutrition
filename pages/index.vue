4279d648279ff30f5980a01f52bd365ec5b5b4ec
Adrien Michaud <adrien.michaud@epitech.eu>
2023-09-11 17:37:20 +0200

    add google balise for gsc

--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,12 @@
         <NuxtLink to="/navbars/design-one"> To navbar blue </NuxtLink>
     </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+    title: 'Honkonutrition',
+    meta: [
+        { name: 'google-site-verification', content: 'JL-HwZP7bD6mQ0AHVyLjnDp4JClYJPkMyanmPiwwvZQ' }
+    ],
+})
+</script>
